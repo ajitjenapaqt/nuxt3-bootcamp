493214bf61c83bb47c07c79aa305f1ed3ed9b254
Ajit Jena <ajit.jena@paqt.com>
2023-03-11 15:24:59 +0100

    Created pages componentes

---
+++ cartrader/components/Car/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img
+          src="https://stimg.cardekho.com/images/carexteriorimages/930x620/Lamborghini/Urus/4418/Lamborghini-Urus-V8/1621927166506/front-left-side-47.jpg"
+          alt=""
+          class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">
+            Lamborghini Urus V8
+          </h1>
+          <p class="text-gray-700">lorem ipsum test test test test</p>
+        </div>
+        <h1 class="mt-auto text-xl">$89,222</h1>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ cartrader/components/Car/Detail/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mt-10">
+      <img
+          src="https://stimg.cardekho.com/images/carexteriorimages/930x620/Lamborghini/Urus/4418/Lamborghini-Urus-V8/1621927166506/front-left-side-47.jpg"
+          class="w-full"
+          alt=""
+      />
+    </div>
+    <h1 class="mt-10 text-4xl">Lamborghini Urus v8</h1>
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">5 seats</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">69,456 miles</p>
+      </div>
+      <div>
+        <p class="font-bold text-2xl">$89,123</p>
+      </div>
+    </div>
+</template>
